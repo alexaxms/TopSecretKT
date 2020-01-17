a52e53e59b3888d789bd04fa3edeefde9be5c070
alexaxms <alexaxmss@hotmail.com>
2020-01-17 14:20:12 -0300

    Initial commit

---
+++ src/main/kotlin/com/rankmi/topsecret/TopsecretApplication.kt
@@ -0,0 +1,11 @@
+package com.rankmi.topsecret
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TopsecretApplication
+
+fun main(args: Array<String>) {
+	runApplication<TopsecretApplication>(*args)
+}
---
+++ src/test/kotlin/com/rankmi/topsecret/TopsecretApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rankmi.topsecret
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TopsecretApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
